0ecf79f2a7b650eb16c2388f70fb730abc08369f
Jelle <j.visser@gmu.online>
2025-01-08 15:53:17 +0100

    Small notification segment

--- app/Filament/Widgets/DashboardInfoWidget.php
+++ app/Filament/Widgets/DashboardInfoWidget.php
@@ -2,10 +2,38 @@
 
 namespace App\Filament\Widgets;
 
+use Filament\Actions\{Action, Concerns\InteractsWithActions, Contracts\HasActions};
+use App\Filament\Resources\AttendeeResource;
+use Filament\Forms\{Concerns\InteractsWithForms, Contracts\HasForms};
+use Filament\Notifications\Notification;
 use Filament\Widgets\Widget;
 
-class DashboardInfoWidget extends Widget
+class DashboardInfoWidget extends Widget implements HasActions, HasForms
 {
-    protected int | string | array $columnSpan = 'full';
+    use InteractsWithActions;
+    use InteractsWithForms;
+
+    protected int|string|array $columnSpan = 'full';
     protected static string $view = 'filament.widgets.dashboard-info-widget';
+
+    public function callNotification(): Action
+    {
+        return Action::make('callNotification')
+            ->button()
+            ->color('warning')
+            ->label('Call Notification')
+            ->action(function () {
+                Notification::make()->warning()
+                    ->title('You have a new notification!')
+                    ->persistent()
+                    ->actions([
+                        \Filament\Notifications\Actions\Action::make('goToAttendees')
+                            ->button()
+                            ->color('primary')
+                            ->url(AttendeeResource::getUrl())
+                    ])
+                    ->body('This is a test notification.')
+                    ->send();
+            });
+    }
 }
--- resources/views/filament/widgets/dashboard-info-widget.blade.php
+++ resources/views/filament/widgets/dashboard-info-widget.blade.php
@@ -1,9 +1,10 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-        <div class="flex justify-center">
+        <div class="flex space-x-6 justify-between">
             <p class="text-xl font-medium italic underline underline-offset-2">
                 This is my Laraconf Dashboard Info Widget, Filament is awesome!
             </p>
+            {{ $this->callNotification() }}
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
